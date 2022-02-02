f7e297ee758baf269cd59b28c1d03fc12458fec0
withenex <brandonpi265@gmail.com>
2022-02-01 20:35:40 -0600

    uplodad a whole bunch of photos of ducks

--- twitter.js
+++ twitter.js
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import Twitter from 'twitter'
 import { promises as fs } from 'fs'
+import _ from 'lodash'
 
 
 const client = new Twitter({
@@ -10,30 +11,60 @@ const client = new Twitter({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
 })
 
-/*
-client.post('statuses/update',{status: "Hello World! from nodeJS"},(e,data)=>{
-    if(e)
-    {
-        console.log(e)
-    }
-    console.log(data)
-})*/
+const duckPaths = ['./duck1.jpg', './duck2.jpg', 'duck3.jpg', 'duck4.jpg', 'duck5.jpg', 'duck6.jpg', 'duck7.jpg']
 
+let buffers = await Promise.all(duckPaths.map(async duck => {
+    return fs.readFile(duck);
+}))
 
-async function sendTweet(record) {
+let tweetThis = {}
+tweetThis.media_ids = await uploadPhotos(buffers)
+
+sendTweet(tweetThis)
+
+export async function sendTweet(record) {
     // Build text response
     // Send pictures
 
-    let buffer = await fs.readFile('./test.jpg')
-    console.log(buffer)
-    let mediaIds = await uploadPhotos([buffer])
+    // I know this looks like garbage
+    // What this line does is extract each media id string from record.media_ids
+    // And then chunks them into groups of four since we can only attach four photos per tweet
+    let media_chunks = _.chunk(record.media_ids.map(media => { return media.media_id_string }), 4)
 
-    client.post('statuses/update', { media_ids: mediaIds[0].media_id_string, status: "This is a photo of a duck" }, (e, data) => {
+    await Promise.all(media_chunks.map(async (media_ids, i) => {
+        let status = {}
+        status.media_ids = media_ids.toString()
+        let isMultiPart = (media_chunks.length > 1) ? true : false
+
+        if (isMultiPart) {
+            status.status = `[${i + 1}/${media_chunks.length}]\n`
+        }
+        else {
+            status.status = "Nothing to see here\n"
+        }
+
+        status.status += "Nothing to see here"
+        console.log(status)
+
+        return new Promise((resolve, reject) => {
+            client.post('statuses/update', status, (e, data) => {
+                if (e) {
+                    reject(e)
+                }
+                else if (data) {
+                    resolve(data)
+                }
+            })
+        })
+    }))
+
+    /*
+    client.post('statuses/update', status, (e, data) => {
         console.log(e, data)
-    })
+    })*/
 }
 
-async function uploadPhotos(buffers) {
+export async function uploadPhotos(buffers) {
     buffers = buffers.map(async buffer => {
         // Get name
         return new Promise((resolve, reject) => {
@@ -42,7 +73,6 @@ async function uploadPhotos(buffers) {
                     reject(e)
                 }
                 else if (media) {
-                    console.log(media)
                     resolve(media)
                 }
             })
@@ -52,5 +82,3 @@ async function uploadPhotos(buffers) {
     let mediaIds = await Promise.all(buffers)
     return mediaIds
 }
-
-sendTweet("e");
